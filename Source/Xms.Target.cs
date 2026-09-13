882662c47051adb42d7cd4b61369c6f05c3c046a
Nick Patrick <npatrick04@gmail.com>
2026-09-13 12:30:30 -0500

    Also update the Xms.Target.cs to UE5.8

--- Source/Xms.Target.cs
+++ Source/Xms.Target.cs
@@ -7,8 +7,8 @@ public class XmsTarget : TargetRules
 	public XmsTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("Xms");
 	}
 }
